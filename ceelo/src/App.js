54f17da759d1413ec4e50a7b25c1ce933f2fc296
Ahsan Abbasi <1abbasia@gmail.com>
2023-03-07 21:24:04 -0500

    started to writte the html, will be changing evryting and working from scratch in react

--- ceelo/src/App.js
+++ ceelo/src/App.js
@@ -4,20 +4,9 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <header><h1>CEE-LO Dice game</h1></header>  
+    <section id= "main"><h2>Welcome To Cee Lo! The street dice game!</h2></section>
+
     </div>
   );
 }
